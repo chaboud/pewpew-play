9b9cde9be683f94f00647c7823c6873e8e3edd3f
chaboud <chaboud@gmail.com>
2026-08-25 19:11:51 +0000

    laser-kitty: idle-wander walks now (k31)

--- laser-kitty/catlab.js
+++ laser-kitty/catlab.js
@@ -8,7 +8,7 @@
 // window.__catlab = {ready, setPose, setSpeed, setYaw}
 
 import * as THREE from 'three';
-import { CatRig, POSES } from './catrig.js?v=k30';
+import { CatRig, POSES } from './catrig.js?v=k31';
 
 const params = new URLSearchParams(location.search);
 const canvas = document.getElementById('scene');
