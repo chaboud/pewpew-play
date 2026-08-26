2e8682b276d90b5c8bcfcf2cfd90fdefd5a0f7a6
chaboud <chaboud@gmail.com>
2026-08-26 05:34:05 +0000

    laser-kitty: little bonus points for prints and sparks (k40)

--- laser-kitty/catlab.js
+++ laser-kitty/catlab.js
@@ -8,7 +8,7 @@
 // window.__catlab = {ready, setPose, setSpeed, setYaw}
 
 import * as THREE from 'three';
-import { CatRig, POSES } from './catrig.js?v=k39';
+import { CatRig, POSES } from './catrig.js?v=k40';
 
 const params = new URLSearchParams(location.search);
 const canvas = document.getElementById('scene');
